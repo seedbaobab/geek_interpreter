04836df147eb88d34177771427cff71e259f5bd2
DevPAMGit <49749709+seedbaobab@users.noreply.github.com>
2023-06-12 09:29:49 +0200

    Nettoyage du fichier.

--- analyzer/lexical/core/automaton/automaton_state_lexical.py
+++ analyzer/lexical/core/automaton/automaton_state_lexical.py
@@ -12,13 +12,7 @@ class AutomatonStateLexical(IAutomatonStateLexical):
 
     def extract_token(self, characters: list[str], position: int, max_position: int, token: Optional[str] = None) \
             -> tuple[bool, int, Optional[str]]:
-        print("POSITION : " + str(position))
-        print("MAX : " + str(max_position))
         if position.__ge__(max_position):
-            if max_position.__ge__(position):
-                print("MAX POSITION REACH ")
-                if self._is_final:
-                    print("STATE IS FINAL")
             return (True, position, token) if self._is_final else (False, position, None)
 
         success: bool = False
@@ -28,22 +22,14 @@ class AutomatonStateLexical(IAutomatonStateLexical):
         while transition_position.__lt__(transition_max) and position.__lt__(max_position) and not success:
             transition: AutomatonTransitionLexical = self.__transitions[transition_position]
             if transition.is_valid(characters[position]):
-                print("TRANSITION VALID ON :" + characters[position])
                 (success, new_position, new_token) = transition.destination.extract_token(
                     characters, (position + 1), max_position, characters[position] if token is None
                     else token + characters[position])
                 if success:
-                    print("NEW POSITION : " + str(new_position))
                     position = new_position
                     token = new_token
             else:
-                print("TRANSITION NOT VALID ON :" + characters[position])
                 transition_position += 1
-
-        if (token is not None and self._is_final) or success:
-            print("AUTOMATON VALID " + str((True, position, token)))
-        else:
-            print("AUTOMATON NOT VALID " + str((False, position, None)))
         return (True, position, token) if (token is not None and self._is_final) or success else (False, position, None)
 
     def add_transition(self, transition: AutomatonTransitionLexical):
