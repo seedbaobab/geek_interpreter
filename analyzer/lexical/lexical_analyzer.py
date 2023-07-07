2c17afeba6eb0c67a2ef84bfc986f14ddcc56131
DevPAMGit <49749709+seedbaobab@users.noreply.github.com>
2023-07-07 09:56:53 +0200

    Nettoyage du code.

--- analyzer/lexical/lexical_analyzer.py
+++ analyzer/lexical/lexical_analyzer.py
@@ -2,25 +2,29 @@ from typing import Optional
 
 from analyzer.lexical.core.automaton.automaton_lexical import AutomatonLexical
 from analyzer.lexical.core.exception.lexical_analyzer_exception import LexicalAnalyzerException
-from analyzer.lexical.lexical.v1.close_parenthesis_lexical import CloseParenthesisLexical
-from analyzer.lexical.lexical.v1.comma_lexical import CommaLexical
 from analyzer.lexical.lexical.v1.identifier_lexical import IdentifierLexical
-from analyzer.lexical.lexical.v1.integer_lexical import IntegerLexical
-from analyzer.lexical.lexical.v1.open_parenthesis_lexical import OpenParenthesisLexical
-from analyzer.lexical.lexical.v1.point_lexical import PointLexical
 from analyzer.lexical.lexical.v1.space_lexical import SpaceLexical
-from analyzer.lexical.lexical.v1.string_lexical import StringLexical
+from analyzer.lexical.lexical.v1.underscore_lexical import UnderscoreLexical
 from analyzer.lexical.model.token import TokenModel
 
 
 class LexicalAnalyzer:
+    """
+    The lexical analyzer class.
+    """
 
     def __init__(self):
-        self.__lexical: list[AutomatonLexical] = [SpaceLexical(), PointLexical(), OpenParenthesisLexical(),
-                                                  CloseParenthesisLexical(), CommaLexical(), IdentifierLexical(),
-                                                  IntegerLexical(), StringLexical()]
+        """
+        Initialize a new instance of 'LexicalAnalyzer' class.
+        """
+        self.__lexical: list[AutomatonLexical] = [SpaceLexical(), IdentifierLexical(), UnderscoreLexical()]
 
-    def analyse(self, command: str):
+    def analyse(self, command: str) -> list[TokenModel]:
+        """
+        Analyze the command for extract a token list.
+        :param command: The command to analyze.
+        :return: The token list for analyzing of the command.
+        """
         character_position: int = 0
         characters: list[str] = [*command]
         character_maximum: int = len(characters)
@@ -45,6 +49,13 @@ class LexicalAnalyzer:
 
     def __extract_token(self, characters: list[str], character_position: int, character_maximum: int) \
             -> Optional[TokenModel]:
+        """
+        Extract a token.
+        :param characters: The command convert in the character list.
+        :param character_position: The position of the analyzer.
+        :param character_maximum: The ma
+        :return: A token model instance or None.
+        """
         lexical_index: int = 0
         token: Optional[TokenModel] = None
         lexical_maximum: int = len(self.__lexical)
@@ -53,5 +64,4 @@ class LexicalAnalyzer:
             token = self.__lexical[lexical_index].extract_token(characters, character_position, character_maximum)
             lexical_index += 1
 
-        print("ANALYZER EXTRACTION QUIT")
         return token
